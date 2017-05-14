bb623d63dde9f34cd390557d84129793048a3f7b
hank9999 <1825355690@qq.com>
2017-05-14 13:49:17 +0800

    更新组件版本
    更新组件版本

--- install.sh
+++ install.sh
@@ -19,19 +19,19 @@ apt-get update
 apt-get install build-essential git gcc g++ make -y
 cd /root
 wget "http://nginx.org/download/nginx-1.11.13.tar.gz"
-wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.gz"
+wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.40.tar.gz"
 wget "https://www.openssl.org/source/openssl-1.1.0e.tar.gz"
 wget "http://zlib.net/zlib-1.2.11.tar.gz"
 git clone https://github.com/cuber/ngx_http_google_filter_module
 git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 tar xzvf nginx-1.11.13.tar.gz
-tar xzvf pcre-8.39.tar.gz
+tar xzvf pcre-8.40.tar.gz
 tar xzvf openssl-1.1.0e.tar.gz
 tar xzvf zlib-1.2.11.tar.gz
 cd nginx-1.11.13
 ./configure \
   --prefix=/opt/nginx-1.11.13 \
-  --with-pcre=../pcre-8.39 \
+  --with-pcre=../pcre-8.40 \
   --with-openssl=../openssl-1.1.0e \
   --with-zlib=../zlib-1.2.11 \
   --with-http_ssl_module \
@@ -54,19 +54,19 @@ rm -rf /root/nginx.conf
 yum install -y gcc gcc-c++ perl make git
 cd /root
 wget "http://nginx.org/download/nginx-1.11.13.tar.gz"
-wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.gz"
+wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.40.tar.gz"
 wget "https://www.openssl.org/source/openssl-1.1.0e.tar.gz"
 wget "http://zlib.net/zlib-1.2.11.tar.gz"
 git clone https://github.com/cuber/ngx_http_google_filter_module
 git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 tar xzvf nginx-1.11.13.tar.gz
-tar xzvf pcre-8.39.tar.gz
+tar xzvf pcre-8.40.tar.gz
 tar xzvf openssl-1.1.0e.tar.gz
 tar xzvf zlib-1.2.11.tar.gz
 cd nginx-1.11.13
 ./configure \
   --prefix=/opt/nginx-1.11.13 \
-  --with-pcre=/root/pcre-8.39 \
+  --with-pcre=/root/pcre-8.40 \
   --with-openssl=/root/openssl-1.1.0e \
   --with-zlib=/root/zlib-1.2.11 \
   --with-http_ssl_module \
